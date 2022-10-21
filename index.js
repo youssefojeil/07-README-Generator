d5abcbcb950c6078566f6c849f277b3df8c6db14
unknown <youssef.ojeil@hotmail.com>
2022-10-21 18:18:51 -0400

    included required packages, set up questions & inquirer prompt

--- index.js
+++ index.js
@@ -1,13 +1,73 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What is your project's name?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Please write a short description of your project",
+        name: "description"
+    },
+    {
+        type: "list",
+        message: "What kind of license should your project have?",
+        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
+        name: "license"
+    },
+    {
+        type: "input",
+        message: "What command should be run to install dependencies?",
+        name: "dependencies"
+    },
+    {
+        type: "input",
+        message: "What command should be run to run tests?",
+        name: "tests"
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about using the repo?",
+        name: "repoInfo"
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about contributing to the repo?",
+        name: "repoContribute"
+    }
+    
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt(questions)
+    .then((response) => {
+     // fs.appendFile("./response.json", JSON.stringify(response, null, "\t"), (err) => {
+       // err ? console.log(err) : console.log("Success!");
+     // })
+      //console.log(response.username, response.email, response.title);
+    })    
+}
 
 // Function call to initialize app
 init();
